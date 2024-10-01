f7619d96f5722e20b55f1ff9ad8fa8b8e92306cb
Master-1-2 <master270902@gmail.com>
2024-10-01 19:23:48 +0530

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -1,15 +1,14 @@
 import { serve } from "https://deno.land/std@0.177.0/http/server.ts";
 
-const IP = "129.154.234.241";  // Replace with your IP
-const PORT = "43210";  // Replace with your port
+const IP = "129.154.227.35";
+const PORT = "43210"; // change above IP PORT ONLY 
 
-// Handler function to forward requests
 async function handler(req) {
   const url = req.url;
   const body = await req.text();
   const path = url.substring(url.indexOf('.dev') + 4);
   const method = req.method;
-  
+
   return await fetch(`http://${IP}:${PORT}${path}`, {
     method: req.method,
     body: method !== 'GET' && method !== 'HEAD' ? body : undefined,
@@ -17,6 +16,4 @@ async function handler(req) {
   });
 }
 
-// Start the server
 serve(handler);
-
