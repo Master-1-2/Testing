5ef83aaf067be0d24731a4df232f53be4c4227b9
Master-1-2 <master270902@gmail.com>
2024-10-01 18:44:37 +0530

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -1,7 +1,7 @@
 import { serve } from "https://deno.land/std@0.177.0/http/server.ts";
 
 const IP = "129.154.234.241";  // Keep the IP as a string
-const PORT = 43210;  // Remove quotes around the PORT to make it a number
+const PORT = "43210";  // Remove quotes around the PORT to make it a number
 
 // Change above IP and PORT only 
 async function handler(req: Request): Promise<Response> {
